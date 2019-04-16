daa623cd6a6cad2962408f06a8a329c6b1ed502a
RufinaMay <rufa-gali@mail.ru>
2019-04-16 10:15:41 +0300

    image normalization added

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,4 @@
-!curl -o data.gz -L "http://kdd.ics.uci.edu/databases/kddcup99/kddcup.data.gz"
+#!curl -o data.gz -L "http://kdd.ics.uci.edu/databases/kddcup99/kddcup.data.gz"
 import os
 import pandas as pd
 import numpy as np
@@ -19,3 +19,14 @@ def create_train_data():
     df = pd.get_dummies(df, prefix=["protocol_type","service","flag"])
 
     df.to_csv('kdd99_train.csv.gz', compression='gzip',index=False)
+    
+
+def image_normalization_mapping(image, from_min, from_max, to_min, to_max):
+    """
+    Map data from any interval [from_min, from_max] --> [to_min, to_max]
+    Used to normalize and denormalize images
+    """
+    from_range = from_max - from_min
+    to_range = to_max - to_min
+    scaled = np.array((image - from_min) / float(from_range), dtype=float)
+    return to_min + (scaled * to_range)
