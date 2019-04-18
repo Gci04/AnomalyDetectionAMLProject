8ad80392a603de7f1c480bafb0be13552fd4a9b0
RufinaMay <rufa-gali@mail.ru>
2019-04-18 20:02:31 +0300

    conv auto enc is done

--- convolutionalAutoEncoder.py
+++ convolutionalAutoEncoder.py
@@ -5,12 +5,11 @@ Created on Tue Apr 16 09:26:07 2019
 @author: Rufina
 """
 import numpy as np
-from preprocessing import image_normalization_mapping
 from keras.datasets import cifar10
 from matplotlib import pyplot as plt
-from keras.models import  Model
+from keras.models import Sequential,  Model
 from keras.layers import Conv2D, LeakyReLU, BatchNormalization, Conv2DTranspose, ReLU, Input, Dense, Reshape, Flatten, Dropout
-from keras.optimizers import Adam, RMSprop
+from keras.optimizers import Adam
 import pickle
 # from preprocessing import image_normalization_mapping
 
@@ -34,7 +33,15 @@ idx = np.where(Y_train[:,0]!=NORMAL_CLASS)[0][:ANOMAL_DATA_NUMB]
 anomal = X_train[idx]
 
 #NORMALIZE DATA
-
+def image_normalization_mapping(image, from_min, from_max, to_min, to_max):
+    """
+    Map data from any interval [from_min, from_max] --> [to_min, to_max]
+    Used to normalize and denormalize images
+    """
+    from_range = from_max - from_min
+    to_range = to_max - to_min
+    scaled = np.array((image - from_min) / float(from_range), dtype=float)
+    return to_min + (scaled * to_range)
 train_normal = image_normalization_mapping(train_normal, 0, 255, -1, 1)
 test_normal = image_normalization_mapping(test_normal, 0, 255, -1, 1)
 anomal = image_normalization_mapping(anomal, 0, 255, -1, 1)
@@ -43,20 +50,22 @@ def get_mse(original, decoded):
     error = np.sum((original - decoded) ** 2)
     error /= IM_SIZE * IM_SIZE
     return error
+
   
 def Convolutional_Autoencoder(lr):
     input_img = Input(shape = (IM_SIZE,IM_SIZE,IM_CHANNELS))
     l1 = Conv2D(filters=16, kernel_size=2, strides=(2,2), input_shape=(IM_SIZE,IM_SIZE,IM_CHANNELS), activation = 'tanh')(input_img)
-    l2 =  Conv2D(filters=32, kernel_size=2, strides=(2,2), activation = 'tanh')(l1)
-    l3 = Conv2D(filters=64, kernel_size=2, strides=(2,2), activation = 'tanh')(l2)
-    l4 =  Conv2D(filters=128, kernel_size=2, strides=(2,2), activation = 'tanh')(l3)
+    l2 =  Conv2D(filters=64, kernel_size=2, strides=(2,2), activation = 'tanh')(l1)
+    l3 = Conv2D(filters=128, kernel_size=2, strides=(2,2), activation = 'tanh')(l2)
+    l4 =  Conv2D(filters=256, kernel_size=2, strides=(2,2), activation = 'tanh')(l3)
     
-    encoding = Conv2D(filters=512, kernel_size=2, strides=(2,2), activation = 'tanh')(l4)
+    encoding = Conv2D(filters=32, kernel_size=2, strides=(2,2), activation = 'tanh')(l4)
     
-    l5 = Conv2DTranspose(filters=128, kernel_size=2, strides=(2,2), activation = 'tanh')(encoding)
-    l6 = Conv2DTranspose(filters=64, kernel_size=2, strides=(2,2), activation = 'tanh')(l5)
-    l7 = Conv2DTranspose(filters=32, kernel_size=2, strides=(2,2), activation = 'tanh')(l6)
-    l8 = Conv2DTranspose(filters=16, kernel_size=2, strides=(2,2), activation = 'tanh')(l7)
+    l5 = Conv2DTranspose(filters=512, kernel_size=2, strides=(2,2), activation = 'tanh')(encoding)
+    l6 = Conv2DTranspose(filters=256, kernel_size=2, strides=(2,2), activation = 'tanh')(l5)
+    l7 = Conv2DTranspose(filters=128, kernel_size=2, strides=(2,2), activation = 'tanh')(l6)
+    l8 = Conv2DTranspose(filters=128, kernel_size=2, strides=(2,2), activation = 'tanh')(l7)
+    
     decoded = Conv2DTranspose(filters=3, kernel_size=2, strides=(2,2), activation = 'tanh')(l8)
     
     autoencoder = Model(inputs=input_img , outputs=decoded)    
@@ -68,33 +77,33 @@ def Convolutional_Autoencoder(lr):
     
     return autoencoder, dim_reducer
 
-# def Classical_autoencoder(lr = 0.01): 
-#     autoencoder = Sequential()
-#     autoencoder.add(Flatten())
-#     autoencoder.add(Dense(128, activation="tanh"))
-#     autoencoder.add(Dense(64, activation="tanh"))
-#     autoencoder.add(Dense(32, activation="tanh"))
-#     autoencoder.add(Dense(4,activation=None))
+def Classical_autoencoder(lr = 0.01): 
+    autoencoder = Sequential()
+    autoencoder.add(Flatten())
+    autoencoder.add(Dense(128, activation="tanh"))
+    autoencoder.add(Dense(64, activation="tanh"))
+    autoencoder.add(Dense(32, activation="tanh"))
+    autoencoder.add(Dense(4,activation=None))
     
     
-#     autoencoder.add(Dense(4, activation="tanh"))
-#     autoencoder.add(Dense(32, activation="tanh"))
-#     autoencoder.add(Dense(64, activation='tanh'))
+    autoencoder.add(Dense(4, activation="tanh"))
+    autoencoder.add(Dense(32, activation="tanh"))
+    autoencoder.add(Dense(64, activation='tanh'))
     
-#     autoencoder.add(Dense(IM_SIZE*IM_SIZE*IM_CHANNELS,activation='tanh'))
-#     autoencoder.add(Reshape((IM_SIZE,IM_SIZE,IM_CHANNELS)))
+    autoencoder.add(Dense(IM_SIZE*IM_SIZE*IM_CHANNELS,activation='tanh'))
+    autoencoder.add(Reshape((IM_SIZE,IM_SIZE,IM_CHANNELS)))
     
-#     inp = Input(shape=(IM_SIZE, IM_SIZE, IM_CHANNELS))
-#     out = autoencoder(inp)
+    inp = Input(shape=(IM_SIZE, IM_SIZE, IM_CHANNELS))
+    out = autoencoder(inp)
     
-#     autoencoder_model = Model(inputs=inp , outputs=out)
-#     autoencoder_model.summary()
-#     optimizer = Adam(lr)
+    autoencoder_model = Model(inputs=inp , outputs=out)
+    autoencoder_model.summary()
+    optimizer = Adam(lr, 0.5)
     
-#     autoencoder_model.compile(loss='mean_squared_error', 
-#         optimizer=optimizer, metrics=['accuracy', get_mse])
+    autoencoder_model.compile(loss='mean_squared_error', 
+        optimizer=optimizer, metrics=['accuracy', get_mse])
     
-#     return autoencoder_model
+    return autoencoder_model
 
    
 def Batches(normal_data, batches_numb):
@@ -108,12 +117,12 @@ def Batches(normal_data, batches_numb):
         data = normal_data[start_idx:end_idx]
         yield data
 
-def train(epochs=501, lr = 0.001):
+def train(epochs=5000, lr = 0.001):
     autoencoder, dim_reducer = Convolutional_Autoencoder(lr)
     for epoch in range(epochs):
         n = 0
         LOSS, ACC, MSE = 0., 0., 0.
-        for normal_data in Batches(train_normal, 10):
+        for normal_data in Batches(train_normal, 1):
             loss, accuracy, mse = autoencoder.train_on_batch(normal_data,normal_data)
             n+=1
             LOSS += loss
@@ -173,4 +182,3 @@ with open('test_mixed.pickle', 'wb') as f:
   
 with open('labels.pickle', 'wb') as f:
   pickle.dump(labels, f)
- 
